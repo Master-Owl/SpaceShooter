bcb0cdc33d32365921b3ba8a73bfb031b78b1db9
Master-Owl <cheesytrevor@gmail.com>
2017-12-22 16:40:34 -0700

    Put asteroid in scene and set up sorting layers

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour {
+
+	public Sprite[] asteroidSprites = new Sprite[4];
+
+	// Use this for initialization
+	void Start () {
+		SpriteRenderer renderer = gameObject.GetComponent<SpriteRenderer>();
+		renderer.sprite = asteroidSprites[Random.Range(0, asteroidSprites.Length)];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
