6f6b99c4aae3a7b2189d32bf7e0b8ffaa4759c54
Master-Owl <cheesytrevor@gmail.com>
2017-12-27 11:20:38 -0700

    Created basic main menu

---
+++ Assets/Scripts/MenuNavigations.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+public class MenuNavigations : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void LoadSceneByName(string name) {
+		SceneManager.LoadScene(name);
+	}
+
+	public void QuitApplication() {
+		if (Application.isEditor)
+			EditorApplication.isPlaying = false;
+		else 
+			Application.Quit();		
+	}
+}
